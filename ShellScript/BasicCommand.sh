2e2d809b0e116edbc1709686a37dfe8d339a28cf
arun789 <adwiti1975@gmail.com>
2017-05-09 21:40:52 -0700

    first commit

---
+++ ShellScript/BasicCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pwd #gives you the current directory path
+ls -lrt # it will display all the files and directories under the current directory
+echo "What is your name"
+read person
+echo "The name of the person, $person"
