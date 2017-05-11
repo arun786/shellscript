ece2985c7ba872447778802d328d716c00a44f13
arun789 <adwiti1975@gmail.com>
2017-05-11 00:46:44 -0700

    Arithmetic operators

--- ShellScript/ArithmeticOperators.sh
+++ ShellScript/ArithmeticOperators.sh
@@ -2,5 +2,22 @@
 a=10
 b=20
 
+# add 2 values
 c=`expr ${a} + ${b}`
 echo ${c} 
+
+# substract 2 values
+c=`expr ${a} - ${b}`
+echo ${c}
+
+# Multiply 2 values
+c=`expr ${a} \* ${b}`
+echo ${c}
+
+#Division of two values
+c=`expr ${b} / ${a}`
+echo ${c}
+
+# Modulus of two values
+c=`expr ${b} % ${a}`
+echo ${c}
