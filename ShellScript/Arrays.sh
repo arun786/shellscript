9915db73f81cbdc9035ab07dfd6d5e2308497728
arun789 <adwiti1975@gmail.com>
2017-05-11 00:28:06 -0700

    Arrays how it works

--- ShellScript/Arrays.sh
+++ ShellScript/Arrays.sh
@@ -3,4 +3,10 @@ name[0]="zero"
 name[1]="one"
 name[2]="two"
 
+# to display all the records in an array the below command can be used
 echo ${name[*]}
+echo ${name[@]} 
+
+# to display a particular value from an array
+
+echo ${name[0]}
