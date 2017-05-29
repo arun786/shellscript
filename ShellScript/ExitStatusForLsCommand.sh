24c0e997a27b322db58b3f3f66e87060ba18fcba
arun789 <adwiti1975@gmail.com>
2017-05-29 15:07:02 -0700

    Non zero code

--- ShellScript/ExitStatusForLsCommand.sh
+++ ShellScript/ExitStatusForLsCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#if the command is successfully executed where ls -lrt is able to find the file Arrays.sh it will return 0 as the status code else 1
+#if the command is successfully executed where ls -lrt is able to find the file Arrays.sh it will return 0 as the status code will be non zero
 ls -lrt Arrays.sh
 
 echo "$?"
