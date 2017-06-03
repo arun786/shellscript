5060cc9582371a2935bcece025999db16bb88f2b
arun789 <adwiti1975@gmail.com>
2017-06-02 23:33:31 -0700

    backup success code

--- ShellScript/functiontoBackUpAfile.sh
+++ ShellScript/functiontoBackUpAfile.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 function backup_a_file(){
 	
 			if [ -f $1 ]
@@ -10,7 +8,13 @@ function backup_a_file(){
 cp ${1} ${backup}
 else
 echo "its not a file"
+return 1
 fi
 }
 
 backup_a_file ${1}
+
+if [ $? -eq 0 ]
+then
+	echo "Backup successful"
+fi
