45602fe9bcdb25de3295f1425518dcb1aadc05e3
arun789 <adwiti1975@gmail.com>
2017-06-07 01:50:21 -0700

    basic commands

---
+++ ShellScript/BasicCommands.sh
@@ -0,0 +1,34 @@
+#to create an empty file
+touch <name of the file>
+
+# to create a file with some specific content
+cat > <name of the file>
+
+# to check current directory
+pwd
+
+#to display files in a directory
+#to display all files and directory
+ls 
+
+# to display all the files with details
+# l means with all details
+ls -l
+
+#to see the hidden files 
+ls -a
+
+#to display files in order such as latest at the top
+ls -lt
+
+#to display files in reverse order latest at bottom
+ls -lrt
+
+#to display files in human readable format
+
+ls -lh
+
+
+
+
+
