b9a0a9bed2ef65ee46affca660f2f5b730708133
arun789 <adwiti1975@gmail.com>
2017-06-08 00:33:35 -0700

    cat commands

---
+++ ShellScript/CATWORK/CatCommands.sh
@@ -0,0 +1,15 @@
+#to create a new file with contents
+
+cat > filename
+
+#to copy the contents of the file from one file to another
+
+cat filename > newFileName
+
+# to copy the contents of two or more file to a new file
+
+cat filename newfileName > data1
+
+#to append the contents of a file to existing file
+
+cat freshfile >> data1
