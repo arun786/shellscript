83312fe892ada216a5fd219a8f68ec4935c28d8f
arun789 <adwiti1975@gmail.com>
2017-06-08 22:45:04 -0700

    grep command

---
+++ ShellScript/GREP/GrepCommand.sh
@@ -0,0 +1,13 @@
+#grep Global regular expression parser
+
+#to find text in a particular file
+
+grep <text> <filename>
+
+#to find text in more than 1 file
+
+grep <text> <filename1> <filename2>
+
+#to find text in current directory
+
+grep <text> *
