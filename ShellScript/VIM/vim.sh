47087ab5c88ec58849ca0fa0e55ff413eed3e048
arun789 <adwiti1975@gmail.com>
2017-06-16 01:08:15 -0700

    vim commands

---
+++ ShellScript/VIM/vim.sh
@@ -0,0 +1,21 @@
+#to insert data to current position - i
+#insert data at the beggining of the line - I
+#to insert data before a line - O
+#to insert data after a line - o
+#to delete a character which is selected - x
+#to delete a line - dd
+#to delete a lines reamining part after the postion of cursor - d
+#to delete multiple line - if you want to delete 2 lines : 2dd
+#to append data to a particular position - a
+#to append at the end of the line - A
+#to search data in a file - :/ = forward search
+#to search data in a file - :?<word> = backword search
+
+n for moving data to next data.
+N for miving to previous data.
+
+#save and quit = esc !wq
+#to quit a file without saving when no modification is done = esc !q
+#to know the current shell, echo $SHELL
+
+
